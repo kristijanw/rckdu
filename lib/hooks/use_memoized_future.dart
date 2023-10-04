b5edaf25894a151f288f19e06f10727916dcef85
dev-prospekt <dev@prospekt.hr>
2023-10-04 10:26:37 +0200

    update

--- lib/hooks/use_memoized_future.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-
-AsyncSnapshot<T> useMemoizedFuture<T>(Future<T> Function() create) {
-  final future = useMemoized(create, const []);
-  return useFuture(future);
-}
